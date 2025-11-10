4eb00ff4749593eb7039190cd3afd8e6b5b8f737
pianisti <192371238+pianisti@users.noreply.github.com>
2025-11-10 22:56:44 +0200

    tehtävä 2

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,24 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    response = requests.get(url).json()
+
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        if player.nationality == "FIN":
+            players.append(player)
+
+    print("Players from FIN:")
+    
+    for player in players:
+        print(player)
 
 if __name__ == "__main__":
     main()
