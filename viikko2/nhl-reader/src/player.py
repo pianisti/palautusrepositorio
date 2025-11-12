246d577204f41aacbe4d3ca3ca28e4be5cc385a2
pianisti <192371238+pianisti@users.noreply.github.com>
2025-11-13 01:32:01 +0200

    tehtävä 8

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,12 +1,12 @@
 class Player:
-    def __init__(self, dict):
-        self.name = dict['name']
-        self.nationality = dict["nationality"]
-        self.assists = dict["assists"]
-        self.goals = dict["goals"]
-        self.team = dict["team"]
-        self.games = dict["games"]
-        self.id = dict["id"]        
-    
+    def __init__(self, player_dict):
+        self.name = player_dict['name']
+        self.nationality = player_dict["nationality"]
+        self.assists = player_dict["assists"]
+        self.goals = player_dict["goals"]
+        self.team = player_dict["team"]
+        self.games = player_dict["games"]
+        self.id = player_dict["id"]
+
     def __str__(self):
         return f"{self.name:20}{self.team:15}{self.goals:2} + {self.assists:2} = {self.goals + self.assists:2}"
--- viikko2/nhl-reader/src/player_reader.py
+++ viikko2/nhl-reader/src/player_reader.py
@@ -5,11 +5,11 @@ class PlayerReader:
     def __init__(self, url):
         self.url = url
         self.players = []
-    
-    def get_players(self):
-        self.response = requests.get(self.url).json()
 
-        for player_dict in self.response:
+    def get_players(self):
+        response = requests.get(self.url).json()
+
+        for player_dict in response:
             player = Player(player_dict)
             self.players.append(player)
 
